9d5bb2b0a130ce80fdcdbcf70ab7720321fdbc35
et41 <erenturan41@gmail.com>
2022-07-28 12:43:25 +0300

    responsive slider

--- components/Slider/slider.js
+++ components/Slider/slider.js
@@ -6,7 +6,8 @@ import data from "../../eventData.json";
 
 
 export default function Slider(  ) {
-		const [sliderIndex, setSliderIndex] = useState(0)
+	const [sliderIndex, setSliderIndex] = useState(0);
+	const [isHover, setIsHover] = useState({0:true, 1:false, 2:false, 3:false, 4:false});
 
 
 	const importantEvents = [];
@@ -27,23 +28,73 @@ export default function Slider(  ) {
 
 	}
 
-	console.log("importantEvents", `/images/event${sliderIndex}.jpg`, checkActive(1) );
-	
+	const hoverHandler = (index) => {
+		console.log("hoverHandler", index)
+		let newHover = {
+			...isHover,
+			[index]: !isHover[index]
+		}
+		console.log("hover", newHover)
+		setIsHover(newHover)
+	}
+
+	console.log("isHover", isHover)
+
 	return (
 		<div className={sliderStyle.slider}>
 			{/*<SliderBtn />
 			<div className={sliderStyle.slider}>
 					<Image key={sliderIndex} src={`/images/event${sliderIndex}.jpg`} layout="fill" className={sliderIndex.image}/>
 			</div>*/}
-			<div className={sliderStyle.slider1} >
-				<Image src={"/images/event0.jpg"} width={100} height={200} />
+			{/*<div onClick={() => hoverHandler(0)} 
+				className={`${sliderStyle.slider1} ${isHover[0] ? sliderStyle.active : sliderStyle.inactive}`}
+			>
+				<Image src={"/images/event0.jpg"} width={`20vw`} height={`20%`} layout={`responsive`} className={sliderStyle.imageActive} />
 			</div>
-			<div className={sliderStyle.slider2} >
-				<Image src={"/images/event1.jpg"} width={100} height={200} />
+			<div onClick={() => hoverHandler(1)}
+				className={`${sliderStyle.slider2} ${isHover[1] ? sliderStyle.active : sliderStyle.inactive}`}
+			>
+				<Image src={"/images/event1.jpg"}  width={`20vw`} height={`20%`} layout={`responsive`}  />
 			</div>
-			<div className={sliderStyle.slider1} >
-				<Image src={"/images/event2.jpg"} width={100} height={200} />
+			<div onClick={() => hoverHandler(2)} 
+				className={`${sliderStyle.slider3} ${isHover[2] ? sliderStyle.active : sliderStyle.inactive}`} >
+				<Image src={"/images/event2.jpg"}  width={`20vw`} height={`20%`} layout={`responsive`} />
 			</div>
+			<div onClick={() => hoverHandler(3)} 
+				className={`${sliderStyle.slider4} ${isHover[3] ? sliderStyle.active : sliderStyle.inactive}`} 
+			>
+				<Image src={"/images/event3.jpg"} width={`20vw`} height={`20%`} layout={`responsive`} />
+			</div>
+			<div onClick={() => hoverHandler(4)} 
+				className={`${sliderStyle.slider5} ${isHover[4] ? sliderStyle.active : sliderStyle.inactive}`} 
+			>
+				<Image src={"/images/event4.jpg"}  width={`20vw`} height={`20%`} layout={`responsive`}  />
+		</div>*/}
+<div onClick={() => hoverHandler(0)} 
+				className={`${sliderStyle.slider1} ${isHover[0] ? sliderStyle.active : sliderStyle.inactive}`}
+			>
+				<img src={"/images/event0.jpg"} className={sliderStyle.sliderImage0}/>
+			</div>
+			<div onClick={() => hoverHandler(1)}
+				className={`${sliderStyle.slider2} ${isHover[1] ? sliderStyle.active : sliderStyle.inactive}`}
+			>
+				<img src={"/images/event1.jpg"} className={sliderStyle.sliderImage1} />
+			</div>
+			<div onClick={() => hoverHandler(2)} 
+				className={`${sliderStyle.slider3} ${isHover[2] ? sliderStyle.active : sliderStyle.inactive}`} >
+				<img src={"/images/event2.jpg"}  className={sliderStyle.sliderImage2}/>
+			</div>
+			<div onClick={() => hoverHandler(3)} 
+				className={`${sliderStyle.slider4} ${isHover[3] ? sliderStyle.active : sliderStyle.inactive}`} 
+			>
+				<img src={"/images/event3.jpg"} className={sliderStyle.sliderImage3} />
+			</div>
+			<div onClick={() => hoverHandler(4)} 
+				className={`${sliderStyle.slider5} ${isHover[4] ? sliderStyle.active : sliderStyle.inactive}`} 
+			>
+				<img src={"/images/event4.jpg"}  className={sliderStyle.sliderImage4}/>
+		</div>
+		
 
 
 		</div>
