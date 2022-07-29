1084a9c94d8f53f16b21ccbf8c6e672cab9facb8
et41 <erenturan41@gmail.com>
2022-07-29 09:03:44 +0300

    slider changes

--- components/Slider/slider.js
+++ components/Slider/slider.js
@@ -8,6 +8,7 @@ import data from "../../eventData.json";
 export default function Slider(  ) {
 	const [sliderIndex, setSliderIndex] = useState(0);
 	const [isHover, setIsHover] = useState({0:true, 1:false, 2:false, 3:false, 4:false});
+	const [scrolled, setScrolled] = useState(0);
 
 
 	const importantEvents = [];
@@ -39,9 +40,32 @@ export default function Slider(  ) {
 	}
 
 	console.log("isHover", isHover)
+	const scrollHandlerToRight = (e) => {
+		let el = document.getElementById("slider")
+		el.scroll({
+			left: scrolled + 400,
+			behavior: "smooth",
 
+		})
+		setScrolled(scrolled + 400)
+
+		console.log("scrollHandler", el.pageX)
+
+	}
+
+	const scrollHandlerToLeft = (e) => {
+		console.log("left scrollHandler", e)
+		let el = document.getElementById("slider")
+		el.scroll({
+			left : scrolled - 400,
+			behavior: "smooth",
+		})
+		setScrolled(scrolled - 400)
+	}
+
+	console.log("scrolled", scrolled)
 	return (
-		<div className={sliderStyle.slider}>
+		<div id="slider" className={sliderStyle.slider}>
 			{/*<SliderBtn />
 			<div className={sliderStyle.slider}>
 					<Image key={sliderIndex} src={`/images/event${sliderIndex}.jpg`} layout="fill" className={sliderIndex.image}/>
@@ -70,10 +94,15 @@ export default function Slider(  ) {
 			>
 				<Image src={"/images/event4.jpg"}  width={`20vw`} height={`20%`} layout={`responsive`}  />
 		</div>*/}
+		<div onClick={scrollHandlerToLeft} className={sliderStyle.slideBarLeft}	 >
+					&#x3c;
+				</div>
+
+
 <div onClick={() => hoverHandler(0)} 
 				className={`${sliderStyle.slider1} ${isHover[0] ? sliderStyle.active : sliderStyle.inactive}`}
 			>
-				<img src={"/images/event0.jpg"} className={sliderStyle.sliderImage0}/>
+				<img src={"/images/event0.jpg"} className={sliderStyle.sliderImage0 }/>
 			</div>
 			<div onClick={() => hoverHandler(1)}
 				className={`${sliderStyle.slider2} ${isHover[1] ? sliderStyle.active : sliderStyle.inactive}`}
@@ -94,8 +123,11 @@ export default function Slider(  ) {
 			>
 				<img src={"/images/event4.jpg"}  className={sliderStyle.sliderImage4}/>
 		</div>
-		
+				<div onClick={scrollHandlerToRight} className={sliderStyle.slideBarRight}	 >
+					&#x3e;
+				</div>
 
+			
 
 		</div>
 	)
